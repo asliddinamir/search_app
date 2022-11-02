113d391277e2aa49ce4c952b9da9013bd5743b80
Asliddin Amirov <asliddinamir2@gmail.com>
2022-11-03 00:36:40 +0300

    search_app

---
+++ script.js
@@ -0,0 +1,32 @@
+let base_url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&prop=extracts&exintro&explaintext&redirects=1&titles='
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+const info = document.querySelector('.info')
+const title = document.querySelector('.title')
+input.addEventListener('keypress', func)
+btn.addEventListener('click', btnFunc)
+
+function func(e) {
+    if (e.keyCode == 13) {
+        btnFunc()
+    }
+}
+
+function btnFunc(){
+    let word = input.value
+    fetch(`${base_url}${word}`)
+    .then(a => a.json())
+    .then(b => {
+        b = b.query.pages
+        let pageId = Object.keys(b)[0]
+        let extract = b[pageId].extract
+        console.log(b);
+        title.innerText = 'Result for ' + `"${word}"`
+        info.innerText = extract
+        if(b[-1].ns === 0){
+            title.innerText = 'No results'
+            info.innerText = ''
+        }
+    })
+
+}
